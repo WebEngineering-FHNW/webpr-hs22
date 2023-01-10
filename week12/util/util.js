101c4333cdf676a3675f201237bce6c34f37552c
Dierk Koenig <dierk.koenig@canoo.com>
2023-01-10 17:14:25 +0100

    after week 14 - with roman numerals in week12 dir

--- week12/util/util.js
+++ week12/util/util.js
@@ -1,5 +1,6 @@
 // https://stackoverflow.com/questions/10993824/do-something-n-times-declarative-syntax
 
+export { arab };
 const timesFunction = function(callback) {
   if( isNaN(parseInt(Number(this.valueOf()))) ) {
     throw new TypeError("Object is not a valid number");
@@ -11,3 +12,37 @@ const timesFunction = function(callback) {
 
 String.prototype.times = timesFunction;
 Number.prototype.times = timesFunction;
+
+/** @private */
+const romanToNumberMap = {
+    'IV': 4,
+    'IX': 9,
+    'XL': 40,
+    'XC': 90,
+    'CD': 400,
+    'CM': 900,
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+};
+
+/**
+ * @param  { !String } roman      - a roman number as a string, mandatory
+ * @return { undefined | Number } - the arabic number or undefined if the roman number is invalid
+ * @throws { Error } if the number is not a valid roman number
+ * @example
+ * arab("MDCXXXII") === 1632
+ */
+const arab = roman => {
+    if (roman.length === 0) return 0;
+
+    for (const [key, value] of Object.entries(romanToNumberMap)) {
+        const len = key.length;
+        if (roman.startsWith(key)) return value + arab(roman.slice(len));
+    }
+    throw new Error('Invalid Roman numeral: ' + roman);
+};
--- week12/util/utilTest.js
+++ week12/util/utilTest.js
@@ -1,5 +1,7 @@
 
-import './util.js';
+import {arab}    from "./util.js";
+
+'./util.js';
 import { Suite } from './test.js';
 
 const util = Suite("util");
@@ -27,4 +29,21 @@ util.test("times-str", assert => {
     assert.is(collect[0], 0);
     assert.is(collect[9], 9);
 
+});
+
+util.test("roman", assert => {
+    assert.is(arab("") ,          0);
+    assert.is(arab("I"),          1);
+    assert.is(arab("II"),         2);
+    assert.is(arab("V" ),         5);
+    assert.is(arab("VI"),         6);
+    assert.is(arab("X" ),         10);
+    assert.is(arab("IV"),         4);
+    assert.is(arab("IX"),         9);
+    assert.is(arab("XL"),         40);
+    assert.is(arab("XC"),         90);
+    assert.is(arab("CD"),         400);
+    assert.is(arab("CM"),         900);
+    assert.is(arab("L" ),         50);
+    assert.is(arab("MDCXXXII"), 1632);
 });
